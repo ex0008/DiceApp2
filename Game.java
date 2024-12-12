1c45f3ceb3d1f32e9e30ea7a221c51db342087ba
ex0008 <ex0008@8CHANG.>
2024-12-12 14:03:50 +0900

    compatible to 2player

--- Game.java
+++ Game.java
@@ -1,23 +1,35 @@
 public class Game {
-	Player p;
-	Dice d;
-
+	Player p1;
+	Player p2;
+	Dice d1;
+	Dice d2;
+	
 	Game() {
-		p = new Player();
-		d = new Dice();
+		p1 = new Player();
+		p2 = new Player();
+		d1 = new Dice();
+		d2 = new Dice();
+
+		p1.name = "Player1";
+		p2.name = "Player2";
 	}
 
 	void start() {
-		p.turn(d);
-		System.out.println(d.me);
+		p1.turn(d1);
+		p2.turn(d2);
+		System.out.println(p1.name()+":"+d1.me);
+		System.out.println(p2.name()+":"+d2.me);
 	}
 
 	void judge() {
-		if (d.me == 6){
-			System.out.println("win!");
+		if (d1.me == d2.me){
+			System.out.println("draw!");
+		}
+		else if (d1.me > d2.me){
+			System.out.println(p1.name()+" win!");
 		}
 		else{
-			System.out.println("lose!");
+			System.out.println(p2.name()+" win!");
 		}
 	}
 }
--- Player.java
+++ Player.java
@@ -1,4 +1,10 @@
 public class Player {
+	String name;
+
+	String name() {
+		return name;
+	}
+	
 	void turn(Dice d) {
 		d.roll();
 	}
